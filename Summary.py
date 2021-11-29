dea968e86c5b5656d3ca58813c2472734b269014
Swaroop R <61423289+SwaroopRavindra@users.noreply.github.com>
2021-11-29 23:39:40 +0530

    Add files via upload

---
+++ Summary.py
@@ -0,0 +1,12 @@
+CUSTOMER_BOOKING = {
+    "van_details": {
+        "small_van": 0,
+        "medium_van": 0,
+        "large_van": 0,
+        "booking_time": None,
+        "rental_duration": None
+    },
+    "camping_details": {
+        "camp_sites": [],
+        "camp_booking_time": None}
+}
